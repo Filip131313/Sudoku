c5a563729a3afb0e35384364872a88c90eb4e023
Filip Koprivica <114360964+Filip131313@users.noreply.github.com>
2024-11-11 14:09:23 +0100

    Update Board.cs

--- Sudoku/Board.cs
+++ Sudoku/Board.cs
@@ -18,7 +18,6 @@ namespace Sudoku
         // ----------------------------------------- Method InitializeBoard ----------------------------------------------
 
         // Ova metoda mi sluzi da mi napravi Array ili Niz koji ima u sebi 9 nizova od 9 polja.
-
         // This method is used to create an array (or matrix) that contains 9 arrays, each with 9 fields.
         public int[,] InitializeBoard()
         {
@@ -34,34 +33,6 @@ namespace Sudoku
             return board;
         }
 
-        // -------------------------------- Method Display -------------------------------------- 
-        //public void Display1()
-        //{
-
-        //    if (board == null)
-        //    {
-        //        Console.WriteLine("Board nije inicijalizovan.");
-        //        return;
-        //    }
-
-        //    for (int i = 0; i < 9; i++)
-        //    {
-        //        if (i % 3 == 0 && i != 0)
-        //        {
-        //            Console.WriteLine("------+-------+------");
-        //        }
-        //        for (int j = 0; j < 9; j++)
-        //        {
-        //            if (j % 3 == 0 && j != 0)
-        //            {
-        //                Console.Write("| ");
-        //            }
-        //            Console.Write(board[i, j] == 0 ? 0 : board[i, j]);
-        //            if (j < 8) Console.Write(" ");
-        //        }
-        //        Console.WriteLine();
-        //    }
-        //}
-        // Kraj metode
+        
     }
 }
